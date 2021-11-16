df35400f585f84290f51ff184d55dbc5c43737b4
TheHarryFer <70009597+TheHarryFer@users.noreply.github.com>
2021-11-17 00:50:41 +0700

    Update db.config.js (#37)

--- Backend/app/config/db.config.js
+++ Backend/app/config/db.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  HOST: process.env.DB_URL || "localhost",
-  USER: "root",
-  PASSWORD: "friendzone",
-  PORT: 3306,
-  DB: "Friendzone",
-  dialect: "mysql",
+  HOST: process.env.DB_URL,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  PORT: process.env.DB_PORT,
+  DB: process.env.DB_NAME,
+  dialect: process.env.DB_TYPE,
   multipleStatements: true,
   connectTimeout: 20000,
   acquireTimeout: 20000
